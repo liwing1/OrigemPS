6a76f9bb018800f14518ac4086b0cc700b4de741
Li Wing <liwramos1@gmail.com>
2022-05-31 15:41:25 -0300

    add simulation template

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,26 @@
 using namespace std;
 using std::vector;
 
+typedef enum{
+    STATE_SIX_CICLES,
+    STATE_FOUR_CICLES,
+    STATE_ACCEL,
+    STATE_BREAK,
+    STATE_END_SIM
+}simulation_states_t;
+
+typedef struct{
+    simulation_states_t state;
+    int timeOutAccel;
+    int timeOutBreak;
+    int timeOutSimul;
+    int cicleCounter;
+}simulation_t;
+
+int cont = 0;
+
+void simulation_state_mng( simulation_t* , Moto* p_moto );
+void process_dynamic_model( void );
 
 int main() {
 
@@ -34,16 +54,111 @@ int main() {
     Battery battery6 = Battery( 666, 50, NULL);
     cps[6].attatchBattery(&battery6);
 
+
     /* Inicializa Moto e a bateria*/
     Moto moto = Moto("ASD-4567", 60, NULL);
-    Battery battery = Battery( 111, 100, &moto);
+    Battery battery = Battery( 000, 85, NULL);
+    moto.attatchBattery(&battery);
 
-    cout << etb.getAmountOfBattTotal() << endl;
+    /* Iniciar Simulacao */
+    simulation_t simulation = {
+        .state = STATE_SIX_CICLES,
+        .timeOutAccel = 180,
+        .timeOutBreak = 10,
+        .timeOutSimul = 1800,
+        .cicleCounter = 0
+    };
 
-    if(battery.getHost())
+    while(simulation.timeOutSimul--)
     {
-        std::cout << "Bateria tem Host!\n";
+        simulation_state_mng(&simulation, &moto);
+        // process_dynamic_model();
     }
 
     return 0;
+}
+
+
+void simulation_state_mng( simulation_t* p_simulation, Moto* p_moto )
+{
+    switch (p_simulation->state)
+    {
+        case STATE_SIX_CICLES:
+        {
+            if( p_simulation->timeOutAccel )
+            {
+                p_moto->setAccelerator(true);
+                p_simulation->timeOutAccel--;
+            }
+            else if( p_simulation->timeOutBreak )
+            {
+                p_moto->setAccelerator(false);
+                p_simulation->timeOutBreak--;
+            }
+            else
+            {
+                p_simulation->timeOutAccel = 180;
+                p_simulation->timeOutBreak = 10;
+                p_simulation->cicleCounter++;
+            }
+
+
+            if( p_simulation->cicleCounter == 6 )
+            {
+                p_simulation->cicleCounter = 0;
+                p_simulation->state = STATE_FOUR_CICLES;
+            }
+            
+            break;   
+        }
+        
+        case STATE_FOUR_CICLES:
+        {
+            if( p_simulation->timeOutAccel )
+            {
+                p_moto->setAccelerator(true);
+                p_simulation->timeOutAccel--;
+            }
+            else if( p_simulation->timeOutBreak )
+            {
+                p_moto->setAccelerator(false);
+                p_simulation->timeOutBreak--;
+                cont += 1;
+            }
+            else
+            {
+                p_simulation->timeOutAccel = 120;
+                p_simulation->timeOutBreak = 12;
+                p_simulation->cicleCounter++;
+            }
+
+
+            if( p_simulation->cicleCounter == 4 )
+            {
+                p_simulation->cicleCounter = 0;
+                p_simulation->state = STATE_ACCEL;
+            }
+
+            break;   
+        }
+
+        case STATE_ACCEL:
+        {
+            break;   
+        }
+
+        case STATE_BREAK:
+        {
+            break;   
+        }
+
+        case STATE_END_SIM:
+        {
+            break;
+        }
+
+        default:
+            break;
+    }
+
 }
